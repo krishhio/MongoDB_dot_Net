cbe0ca338beec2f5e6f3c0b746748847da9ad1f7
crishhio <crishhio@gmail.com>
2018-10-30 09:39:59 -0600

    Primer ejercicio de integracion MongoDB C#

--- M101DotNet/Program.cs
+++ M101DotNet/Program.cs
@@ -18,11 +18,19 @@ namespace M101DotNet
         static async Task MainAsync(string[] args)
         {
             //await Task.Delay(500);
-            var connectionString = "mongodb://localhost:27017";
-            var client = new MongoClient(connectionString);
-            var db = client.GetDatabase("test");
-            var col = db.GetCollection<BsonDocument>("people");
-            Console.WriteLine("Conexion a Mongodb");
+            /* var connectionString = "mongodb://127.0.0.1:27017";
+             var client = new MongoClient(connectionString);
+             var db = client.GetDatabase("test");
+             var col = db.GetCollection<BsonDocument>("people");
+             Console.WriteLine("Conexion a Mongodb");*/
+            BsonDocument doc = new BsonDocument
+            {
+                {"name","Cristian"}
+            };
+
+            doc.Add("Привет! Я ", 40);
+            doc["profesion"] = "Solution Architect";
+            Console.WriteLine(doc);
 
         }
     }
