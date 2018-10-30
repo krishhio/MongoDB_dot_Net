b366734cf744a123beda222afcecb6a0b3ceaf5c
Cristian Gomez <crishhio@gmail.com>
2018-10-29 23:10:58 -0600

    Creacion de primer ejemplo de conexion con MongoDB en mac

---
+++ M101DotNet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace M101DotNet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+            Console.WriteLine();
+            Console.WriteLine("Presiona Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            //await Task.Delay(500);
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase("test");
+            var col = db.GetCollection<BsonDocument>("people");
+            Console.WriteLine("Conexion a Mongodb");
+
+        }
+    }
+}
